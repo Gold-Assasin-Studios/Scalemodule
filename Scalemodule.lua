6872dfc23b641dd5e36c8102115eca4bcce62de2
fiftyseven <165079833+fiftys7vencode@users.noreply.github.com>
2024-07-12 21:39:34 +0200

    Update Scalemodule.lua

--- Scalemodule.lua
+++ Scalemodule.lua
@@ -3,31 +3,36 @@
 
 local scalemodule = {}
 
-local function convert_to_scale(udim2, parent)
-	local xScale = udim2.X.Scale + (udim2.X.Offset / parent.AbsoluteSize.X)
-	local yScale = udim2.Y.Scale + (udim2.Y.Offset / parent.AbsoluteSize.Y)
-	return UDim2.new(xScale, 0, yScale, 0)
+local function adjust_scale_and_offset(udim2, parent)
+    local xScale = udim2.X.Scale
+    local xOffset = udim2.X.Offset
+    local yScale = udim2.Y.Scale
+    local yOffset = udim2.Y.Offset
+    local adjustedXScale = xScale + (xOffset / parent.AbsoluteSize.X)
+    local adjustedYScale = yScale + (yOffset / parent.AbsoluteSize.Y)
+    
+    return UDim2.new(adjustedXScale, xOffset, adjustedYScale, yOffset)
 end
 
-function scale_ui_instance(instance:Instance)
-	if instance:IsA("GuiObject") then
-		instance.Size = convert_to_scale(instance.Size, instance.Parent)
-		instance.Position = convert_to_scale(instance.Position, instance.Parent)
-	end
+local function scale_ui_instance(instance)
+    if instance:IsA("GuiObject") then
+        instance.Size = adjust_scale_and_offset(instance.Size, instance.Parent)
+        instance.Position = adjust_scale_and_offset(instance.Position, instance.Parent)
+    end
 
-	for _, child in pairs(instance:GetChildren()) do
-		scale_ui_instance(child)
-	end
+    for _, child in pairs(instance:GetChildren()) do
+        scale_ui_instance(child)
+    end
 end
 
-function scalemodule.scale_all_ui_instances(screengui:ScreenGui)
-	if not screengui:IsA("ScreenGui") then
-		error("The provided instance is not a ScreenGui")
-	end
-	
-	for _, instance in pairs(screengui:GetChildren()) do
-		scale_ui_instance(instance)
-	end
+function scalemodule.scale_all_ui_instances(screengui)
+    if not screengui:IsA("ScreenGui") then
+        error("The provided instance is not a ScreenGui")
+    end
+    
+    for _, instance in pairs(screengui:GetChildren()) do
+        scale_ui_instance(instance)
+    end
 end
 
 return scalemodule
